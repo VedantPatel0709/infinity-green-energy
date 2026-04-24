fbb45564df1639c8597304fafb93b3484d49bd5d
Vedant Patel <patelvedant0709@gmail.com>
2026-04-25 02:56:17 +0530

    enable static export

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true, // required for static export
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
